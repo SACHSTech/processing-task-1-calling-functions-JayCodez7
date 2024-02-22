20471e242e422201a80c02b6d55ebae8739d34f4
JayCodez7 <Jayden.lau26@ycdsbk12.ca>
2024-02-22 18:01:45 -0500

    House picture

--- Sketch.java
+++ Sketch.java
@@ -16,20 +16,51 @@ public class Sketch extends PApplet {
    * values here i.e background, stroke, fill etc.
    */
   public void setup() {
-    background(210, 255, 173);
+    background(17, 176, 250);
   }
 
   /**
    * Called repeatedly, anything drawn to the screen goes here
    */
   public void draw() {
-	  
-	// sample code, delete this stuff
-    stroke(128);
-    line(150, 25, 270, 350);  
 
-    stroke(255);
-    line(50, 125, 70, 50);  
+	// Field
+    fill(0, 148, 7);
+    ellipse(125, 400, 400, 250);
+
+  // Draw house
+  
+    // base
+    fill(117, 51, 0);
+    rect(200, 200, 200, 200);
+
+    // roof
+    fill(168, 35, 12);
+    triangle(200, 200, 400, 200, 300, 125);
+
+    // windows
+    fill(171, 200, 255);
+    rect(220, 230, 50, 50);
+
+    fill(171, 200, 255);
+    rect(330, 230, 50, 50);
+
+    line(245, 230, 245, 280);
+    line(220, 255, 270, 255);
+
+    line(355, 230, 355, 280);
+    line(330, 255, 380, 255);
+
+    // door
+    fill(168, 95, 0);
+    rect(275, 300, 50, 115);
+
+    // doorknob
+    ellipse(315, 350, 7, 7);
+
+  // Draw sun
+    fill(255, 230, 0);
+    ellipse(50, 50, 75, 75);
   }
   
   // define other methods down here.
